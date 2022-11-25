870cd63ea9e7c719b2bc6cb9db47c123d6db5bea
kip-i <kppisj@gmail.com>
2022-11-25 18:11:18 +0900

    header,footer追加

--- meijoHackU2022/src/App.tsx
+++ meijoHackU2022/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -27,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer/>
     </div>
   )
 }
