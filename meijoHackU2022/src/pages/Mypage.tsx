50ada7544012a5ede2bf9423fd7b710a7ff1a773
kip-i <kppisj@gmail.com>
2022-11-25 15:40:11 +0900

    [wip]まだサイズ感があんま

--- meijoHackU2022/src/pages/Mypage.tsx
+++ meijoHackU2022/src/pages/Mypage.tsx
@@ -7,6 +7,8 @@ import {
     Grid,
     GridItem,
     Progress,
+    CircularProgress, 
+    CircularProgressLabel
 } from '@chakra-ui/react';
 import { Image } from '@chakra-ui/react'
 import reactLogo from '../assets/react.svg'
@@ -14,7 +16,7 @@ import '../App.css'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import PostButton from '../components/PostButton';
-import theme from '../theme'
+import theme from '../theme';
 
 
 
@@ -33,23 +35,28 @@ function Mypage() {
   */
   return (
     <Box style={{textAlign: 'center'}}>
-      <Box style={{padding:'10px'}}>
-        <Image src={reactLogo} alt='Avatar' style={{margin: '0 auto', border:'3px solid'}} boxSize={'8rem'} />
-      </Box>
       <Text fontSize='4xl'>{name}</Text>
-      <Text fontSize='4xl'>Lv.{level}</Text>
+      <Box style={{padding:'10px'}}>
+        <Image src={reactLogo} alt='Avatar' style={{margin: '0 auto', border:'3px solid'}} boxSize={'100%'} />
+      </Box>
+      
+      
       <Box style={{width:'80%', margin:'0 auto'}} >
-        <Progress value={(totalExperience%perLevelup)} max={perLevelup} colorScheme={'twitter'} />
+        {/*<Progress value={(totalExperience%perLevelup)} max={perLevelup} colorScheme={'twitter'} />
         <Text style={{padding:'0.4rem'}} >
           総経験値 : {totalExperience%perLevelup},
           あと : {perLevelup-(totalExperience%perLevelup)}
-        </Text>
-        
+        </Text>*/}
+        <CircularProgress value={(totalExperience%perLevelup)} max={perLevelup} color={theme.colors.main} size={'100%'}  >
+          <CircularProgressLabel>
+          <Text fontSize='4xl'>Lv.{level}</Text>
+            <Text style={{padding:'0.4rem'}} fontSize='2xl' >
+              総経験値 : {totalExperience%perLevelup},<br/>
+              あと : {perLevelup-(totalExperience%perLevelup)}
+            </Text>
+          </CircularProgressLabel>
+        </CircularProgress>
       </Box>
-      <Box style={{borderTop:'3px solid',borderBottom:'3px solid',width:'90%',margin:'10px auto'}}>
-        <Text fontSize='2xl'>過去の投稿</Text>
-      </Box>
-      <PostButton/>
       <Footer/>
     </Box>
   )
